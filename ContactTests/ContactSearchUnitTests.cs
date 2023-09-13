4561b3ea3e2b55d16e47834f227facd61577f0d5
Danilo Vityk <vityk.dn@gmail.com>
2023-09-13 12:17:30 +0300

    Add project files.

---
+++ ContactTests/ContactSearchUnitTests.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Activities;
+using NUnit.Framework;
+using FakeXrmEasy;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using WorkflowOne;
+using System.Collections.Generic;
+
+namespace TestWorkflowOne
+{
+    [TestFixture]
+    public class ContactSearchTests
+    {
+        [Test]
+        public void OneContactReturned_StatusCodeShouldBe1()
+        {
+            var context = new XrmFakedContext();
+            var inputs = CreateInputParameters();
+
+            var contact = new Entity("contact")
+            {
+                Id = Guid.NewGuid(),
+                ["emailaddress1"] = "test@test.com",
+                ["firstname"] = "Test"
+            };
+
+            var orgService = context.GetOrganizationService();
+
+            context.Initialize(contact);
+
+            var outputParms = context.ExecuteCodeActivity<ContactSearch>(inputs);
+
+            Assert.AreEqual(1, outputParms["Status"]);
+            Assert.AreEqual(contact.Id.ToString(), outputParms["ContactLink"]);
+        }
+        [Test]
+        public void NoContactReturned_StatusCodeShouldBe2()
+        {
+            var context = new XrmFakedContext();
+            var inputs = CreateInputParameters();
+
+
+            var outputParms = context.ExecuteCodeActivity<ContactSearch>(inputs);
+
+            Assert.AreEqual(2, outputParms["Status"]);
+        }
+        [Test]
+        public void MoreThanOneIdenticalContactReturned_StatusCodeShouldBe3()
+        {
+            var context = new XrmFakedContext();
+            var inputs = CreateInputParameters();
+
+            var contact1 = new Entity("contact")
+            {
+                Id = Guid.NewGuid(),
+                ["emailaddress1"] = "test@test.com",
+                ["firstname"] = "Test"
+            };
+
+            var contact2 = new Entity("contact")
+            {
+                Id = Guid.NewGuid(),
+                ["emailaddress1"] = "test@test.com",
+                ["firstname"] = "Test"
+            };
+
+            context.Initialize(new List<Entity> { contact1, contact2 });
+
+            var outputParms = context.ExecuteCodeActivity<ContactSearch>(inputs);
+
+            Assert.AreEqual(3, outputParms["Status"]);
+        }
+
+
+        private Dictionary<string, object> CreateInputParameters()
+        {
+            return new Dictionary<string, object>
+             {
+                 { "Parameter1", "Test" },
+                 { "FieldName1", "firstname" },
+                 { "Parameter2", "test@test.com" },
+                 { "FieldName2", "emailaddress1" }
+             };
+        }
+
+
+    }
+}
---
+++ WorkflowOne/ContactSearch.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Activities;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace WorkflowOne
+{
+    public class ContactSearch : CodeActivity
+    {
+        [Input("Parameter 1")]
+        public InArgument<string> Parameter1 { get; set; }
+
+        [Input("Field Name for Parameter 1")]
+        public InArgument<string> FieldName1 { get; set; }
+
+        [Input("Parameter 2")]
+        public InArgument<string> Parameter2 { get; set; }
+
+        [Input("Field Name for Parameter 2")]
+        public InArgument<string> FieldName2 { get; set; }
+
+        [Output("Status")]
+        public OutArgument<int> Status { get; set; }
+
+        [Output("Contact Link")]
+        public OutArgument<string> ContactLink { get; set; }
+        protected override void Execute(CodeActivityContext context)
+        {
+            IOrganizationServiceFactory serviceFactory = context.GetExtension<IOrganizationServiceFactory>();
+
+            IOrganizationService service = serviceFactory.CreateOrganizationService(null);
+
+            string parameter1 = Parameter1.Get(context);
+            string fieldName1 = FieldName1.Get(context);
+            string parameter2 = Parameter2.Get(context);
+            string fieldName2 = FieldName2.Get(context);
+
+            QueryExpression query = new QueryExpression("contact");
+            query.Criteria.AddCondition(fieldName1, ConditionOperator.Equal, parameter1);
+            query.Criteria.AddCondition(fieldName2, ConditionOperator.Equal, parameter2);
+
+            EntityCollection result = service.RetrieveMultiple(query);
+
+            if (result.Entities.Count == 1)
+            {
+                Status.Set(context, 1);
+                ContactLink.Set(context, result.Entities[0].Id.ToString());
+            }
+            else if (result.Entities.Count == 0)
+            {
+                Status.Set(context, 2);
+            }
+            else
+            {
+                Status.Set(context, 3);
+            }
+        }
+    }
+}
